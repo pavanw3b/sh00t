5c4533cb6bf632e503a0bd47817af4587d96b024
rakshita.manjunatha <rakshita.manjunatha@servicenow.com>
2018-07-01 15:07:11 +0530

    Removed the old  django unwanted imports

--- sh00t/urls.py
+++ sh00t/urls.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.conf import settings
 from app import views
 from django.conf.urls.static import static
