27e7818c207ee0a58968888cb32c42971fbf0b64
pavanw3b <pavanw3b@gmail.com>
2018-10-07 21:11:47 +0530

    Django 2.0.8 uptake

--- sh00t/settings.py
+++ sh00t/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'f6l3gz5xxeg($j*15%_q8nvx0mvp*9h$$^^!4a_q3-dw*2n(&%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
