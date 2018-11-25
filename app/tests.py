fbdb8159c1ff5ca51443fc14061448a73f571572
Pavan <pavanw3b@gmail.com>
2018-11-25 19:44:22 +0530

    Basic unit testcases for models. More to come

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Project, Assessment
 
-# Create your tests here.
+
+class ProjectModelTests(TestCase):
+    new_project_name = "Project created by automated testing"
+    new_flag_name = "Flag created by automated testing"
+    new_sh0t_name = "Sh0t created by automated testing"
+    new_assessment_name = "Assessment created by automated testing"
+
+    def test_can_create_project(self):
+        new_project = Project.objects.create(name=self.new_project_name)
+        new_project.save()
+        self.assertEqual(new_project.name, self.new_project_name)
+
+
+    def test_can_create_assessment(self):
+        new_project = Project.objects.create(name=self.new_project_name)
+        new_project.save()
+
+        new_assessment = Assessment.objects.create(name=self.new_assessment_name, project=new_project)
+        new_assessment.save()
+        self.assertEqual(new_assessment.name, self.new_assessment_name)
+
